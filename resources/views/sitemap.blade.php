4f68765963bb1814b69a32c7457d80c8308b547a
isma <ismaharo18@gmail.com>
2020-10-10 14:11:58 -0500

    updated sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -23,6 +23,9 @@
                     <div class="name"><a href="/">Inicio</a></div>
                 </li>
                 <li>
+                    <div class="name"><a href="/servicio-de-seo">Servicio De SEO En México</a></div>
+                </li>
+                <li>
                     <div class="name"><a href="/posicionamiento-seo/como-hacer-que-mi-pagina-web-aparezca-en-google">¿Cómo hacer que mi página web aparezca en Google?</a></div>
                 </li>
                 <li>
